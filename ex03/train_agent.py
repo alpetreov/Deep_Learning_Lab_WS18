4ed7df930091de960974c0cc8f863d18c003bd1e
Alexander Petrov <alex_petrov13@abv.bg>
2018-11-27 15:57:33 +0100

    update

--- ex03/train_agent.py
+++ ex03/train_agent.py
@@ -32,36 +32,32 @@ def read_data(datasets_dir="./data", frac = 0.1):
     return X_train, y_train, X_valid, y_valid
 
 
-def preprocessing(X_train, y_train, X_valid, y_valid, history_length=1):
+def preprocessing(X_batch, y_batch, history_length=1):
 
     # TODO: preprocess your data here.
     # 1. convert the images in X_train/X_valid to gray scale. If you use rgb2gray() from utils.py, the output shape (96, 96, 1)
     # 2. you can either train your model with continous actions (as you get them from read_data) using regression
     #    or you discretize the action space using action_to_id() from utils.py. If you discretize them, you'll maybe find one_hot() 
     #    useful and you may want to return X_train_unhot ... as well.
-	# create two empty lists in order to store the discretized actions
+    
+    # create two empty lists in order to store the discretized actions
     y_new = []
-    y_new_valid = []
     # use the action_to_id function in loop to discretize the labels
-    for i in range(y_train.shape[0]):
-        y_new.append(action_to_id(y_train[i]))
-    for i in range(y_valid.shape[0]):    
-        y_new_valid.append(action_to_id(y_valid[i]))
+    for i in range(y_batch.shape[0]):
+        y_new.append(action_to_id(y_batch[i]))
     # cast the lists to np arrays
     y_new = np.array(y_new)
-    y_new_valid = np.array(y_new_valid)
     # one hot encode the labels for easier classification
-    y_train = one_hot(y_new)
-    y_valid = one_hot(y_new_valid)
+    y_prep = one_hot(y_new)
     
     # Change X_train and X_valid to gray scale
-    X_train = rgb2gray(X_train)
-    X_valid = rgb2gray(X_valid)
+    X_prep = rgb2gray(X_batch)
+    
     # History:
     # At first you should only use the current image as input to your network to learn the next action. Then the input states
     # have shape (96, 96,1). Later, add a history of the last N images to your state so that a state has shape (96, 96, N).
     
-    return X_train, y_train, X_valid, y_valid
+    return X_prep, y_prep
 
 
 def train_model(X_train, y_train, X_valid, y_valid, n_minibatches, batch_size, lr, model_dir="./models", tensorboard_dir="./tensorboard"):
@@ -77,7 +73,7 @@ def train_model(X_train, y_train, X_valid, y_valid, n_minibatches, batch_size, l
     agent = Model(lr, tensorboard_eval.sess)
     
     num_examples = X_train.shape[0]
-
+	X_valid, y_valid = preprocessing(X_valid, y_valid)
     # TODO: implement the training
     # 
     # 1. write a method sample_minibatch and perform an update step
@@ -91,7 +87,7 @@ def train_model(X_train, y_train, X_valid, y_valid, n_minibatches, batch_size, l
             _, c = sess.run([optimizer, loss], feed_dict={X: X_batch, y: y_batch})
             avg_loss += c / (num_examples // batch_size)
             
-        acc_train = accuracy.eval(feed_dict={X: X_train, y: y_train})
+        acc_train = accuracy.eval(feed_dict={X: X_batch, y: y_batch})
         acc_valid = accuracy.eval(feed_dict={X: X_valid, y: y_valid})
         tensorboard_eval.write_episode_data(epoch, {"loss": avg_loss, "acc_train": acc_train, "acc_valid": acc_valid})
         print("Epoch:",epoch+1, "Train accuracy:", acc_train, "valid accuracy:", acc_valid, "loss:", avg_loss) 
@@ -103,16 +99,14 @@ def train_model(X_train, y_train, X_valid, y_valid, n_minibatches, batch_size, l
 def sample_minibatch(X_train, y_train, iteration, batch_size):
     X_train_batch = X_train[iteration*batch_size:iteration*batch_size+batch_size]
     y_train_batch = y_train[iteration*batch_size:iteration*batch_size+batch_size]
-    return X_train_batch, y_train_batch
+	X_train_batch_p, y_train_batch_p = preprocessing(X_train_batch, y_train_batch)
+    return X_train_batch_p, y_train_batch_p
 
 if __name__ == "__main__":
 
     # read data    
     X_train, y_train, X_valid, y_valid = read_data("./data")
 
-    # preprocess data
-    X_train, y_train, X_valid, y_valid = preprocessing(X_train, y_train, X_valid, y_valid, history_length=1)
-
     # train model (you can change the parameters!)
-    train_model(X_train, y_train, X_valid, n_minibatches=10, batch_size=50, lr=0.0001)
+    train_model(X_train, y_train, X_valid, y_valid, n_minibatches=10, batch_size=50, lr=0.0001)
  
