f30def9c224618fdc7d55de4a43ad1f83709d716
Alexander Petrov <alex_petrov13@abv.bg>
2018-11-27 16:15:26 +0100

    io

--- ex03/train_agent.py
+++ ex03/train_agent.py
@@ -84,7 +84,7 @@ def train_model(X_train, y_train, X_valid, y_valid, n_minibatches, batch_size, l
         for iteration in range(num_examples // batch_size):
             #this cycle is for dividing step by step the heavy work of each neuron
             X_batch, y_batch = sample_minibatch(X_train, y_train, iteration, batch_size)
-            _, c = agent.sess.run([optimizer, loss], feed_dict={X: X_batch, y: y_batch})
+            _, c = agent.sess.run([agent.optimizer, agent.loss], feed_dict={X: X_batch, y: y_batch})
             avg_loss += c / (num_examples // batch_size)
             
         acc_train = accuracy.eval(feed_dict={X: X_batch, y: y_batch})
